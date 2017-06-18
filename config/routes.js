9df8e89bf0b2682827d302bcc94f4701e4b86542
Lea Anthony <lea.anthony@gmail.com>
2017-06-18 11:59:19 +1000

    add GET for invite

--- config/routes.js
+++ config/routes.js
@@ -2,7 +2,10 @@ exports['default'] = {
   routes: function (api) {
     return {
 
-
+      get: [{
+          path: '/invite',
+          action: 'invite'
+        }],
       post: [{
           path: '/login',
           action: 'login'
